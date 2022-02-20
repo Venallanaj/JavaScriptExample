496bf3f3df834165d491824a985fc40fec6f1fd6
Vena <venetike.llanaj@hideolab.al>
2022-02-20 22:50:02 +0100

    changes

---
+++ variable01.js
@@ -0,0 +1,48 @@
+let animal = 'dog',
+    isHungry = 'true',
+    isYoung = 'true',
+    age = 12;
+
+console.log(animal);
+if (isHungry) {
+    console.log("It's " + isHungry + "Yes he's hungry")
+}
+
+if (isYoung) {
+    console.log("if is " + isYoung + " years old he is very young")
+}
+
+
+let firstName = "Vena";
+lastName = prompt("What is his last name?");
+console.log(firstName + " " + lastName);
+alert(firstName + " " + lastName);
+
+
+//va
+var testVar = 2; // Declaration & Assignment
+testVar = 3; // Assignment (mutation)
+testVarB = 3; // Declaration & Assignment
+var testVar = 3; // Re-Declaration & Assignment
+
+
+
+
+//let
+
+let testVarC = 4;
+//let testVarC = 5;
+testVarC = 5;
+
+// Cannot redecale a let
+
+
+//const
+const testVarD = 5;
+//testVarD = 8; 
+//Cannot reassign or redeclare a const
+
+
+let testVareE; // undefinde
+testVareE = null; // null
+console.log(testVareE);
---
+++ variables.js
@@ -0,0 +1,20 @@
+const jordans = 103;
+const name = " Lil Tech(nology)";
+const surname = 'Llanaj';
+const kuti = "Ne  kuti une kam vendosur syze";
+console.log(kuti);
+console.log(surname);
+console.log(name);
+console.log(jordans);
+
+let carName = 'Mercedens';
+document.write(carName);
+
+const person = "Vena llanaj",
+    age = 25,
+    proffesion = 'frontend developer';
+
+console.log(person + " " + age + " " + "My work is: ", proffesion);
+
+let job;
+console.log(job);
